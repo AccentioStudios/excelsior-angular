78fa9834853810a2356e525cd556045ec88eb2f6
alvarogabrielgomez <alvaro@accentiostudios.com>
2024-08-27 16:52:06 -0300

    feat(storybook): add logo to docs

---
+++ projects/excelsior-angular/.storybook/theme.ts
@@ -0,0 +1,10 @@
+import { create } from '@storybook/theming/create'
+import logoUrl from './logo.svg'
+
+export default create({
+  base: 'dark',
+  brandTitle: 'Excelsior Angular',
+  brandUrl: 'https://excelsior-angular.accentio.dev',
+  brandImage: logoUrl,
+  brandTarget: '_self',
+})
