0a9972bbae0293985850452defc50548f03f6c8a
alvarogabrielgomez <alvaro@accentiostudios.com>
2024-08-27 15:08:46 -0300

    feat(storybook): added package version to storybook page

--- projects/excelsior-angular/.storybook/preview.ts
+++ projects/excelsior-angular/.storybook/preview.ts
@@ -1,10 +1,23 @@
 import type { Preview } from '@storybook/angular'
 import { setCompodocJson } from '@storybook/addon-docs/angular'
 import docJson from '../documentation.json'
+import { version } from '../package.json'
+const [major, minor, patch, postfix] = version.split('.')
+
 setCompodocJson(docJson)
 
 const preview: Preview = {
   parameters: {
+    version: {
+      major,
+      minor,
+      patch,
+      postfix,
+      style: {
+        color: '#fff',
+        'font-size': '1rem',
+      },
+    },
     controls: {
       matchers: {
         color: /(background|color)$/i,
