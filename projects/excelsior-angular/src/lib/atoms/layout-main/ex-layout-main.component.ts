6afbdb5cc386b1c6110aff58286edf70464c605b
alvarogabrielgomez <alvaro@accentiostudios.com>
2024-08-27 16:32:32 -0300

    feat(exlayout): starting to design the ex-layout-main

---
+++ projects/excelsior-angular/src/lib/atoms/layout-main/ex-layout-main.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core'
+
+@Component({
+  selector: 'ex-layout-main',
+  templateUrl: './ex-layout-main.component.html',
+  styleUrls: ['./ex-layout-main.component.scss'],
+  standalone: true,
+})
+export class ExLayoutMainComponent implements OnInit {
+  constructor() {}
+  ngOnInit(): void {}
+}
