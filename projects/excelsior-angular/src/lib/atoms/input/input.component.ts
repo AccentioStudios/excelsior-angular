126e19e67e0e844543741ac4495a0678435b3b1d
alvarogabrielgomez <alvaro@accentiostudios.com>
2024-09-10 21:21:47 -0300

    change names of input

--- projects/excelsior-angular/src/lib/atoms/input/input.component.ts
+++ projects/excelsior-angular/src/lib/atoms/input/input.component.ts
@@ -1,7 +1,7 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core'
 
 @Component({
-  selector: 'app-input',
+  selector: 'ex-input',
   templateUrl: './input.component.html',
   styleUrls: ['./input.component.css'],
   standalone: true,
