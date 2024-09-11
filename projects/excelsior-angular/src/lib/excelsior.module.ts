e95fa7e7867b699e095c879ccf032a7dea235b74
alvarogabrielgomez <alvaro@accentiostudios.com>
2024-09-11 15:29:56 -0300

    fix: exporting all the module at once  alpha 9.1.1

--- projects/excelsior-angular/src/lib/excelsior.module.ts
+++ projects/excelsior-angular/src/lib/excelsior.module.ts
@@ -1,5 +1,4 @@
 import { NgModule } from '@angular/core'
-import { CommonModule } from '@angular/common'
 import { ExBadgeComponent } from './atoms/badge/badge.component'
 import { ExIconComponent } from './atoms/icon/ex-icon.component'
 import { ExButtonComponent } from './atoms/button/ex-button.component'
