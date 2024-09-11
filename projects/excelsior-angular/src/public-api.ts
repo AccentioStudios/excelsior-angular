4c908c3ac2721d8de677875c138800f04e0354d5
alvarogabrielgomez <alvaro@accentiostudios.com>
2024-09-11 17:14:38 -0300

    fix: exporting all the module at once  alpha 9.1.2

--- projects/excelsior-angular/src/public-api.ts
+++ projects/excelsior-angular/src/public-api.ts
@@ -2,20 +2,30 @@
  * Public API Surface of excelsior-angular
  */
 
-// export * from './lib/excelsior-angular.service';
-// export * from './lib/excelsior-angular.component';
-
+// Atoms
 export * from './lib/atoms/badge/badge.component'
 export * from './lib/atoms/button/ex-button.component'
-export * from './lib/atoms/icon/ex-icon.component'
 export * from './lib/atoms/card/card.component'
 export * from './lib/atoms/checkbox/checkbox.component'
 export * from './lib/atoms/checkbox-group/checkbox-group.component'
 export * from './lib/atoms/collapsable/collapsible.component'
-export * from './lib/atoms/dropdown/dropdown.component'
+export * from './lib/atoms/dropdown-custom/dropdown.component'
 export * from './lib/atoms/icon/ex-icon.component'
+export * from './lib/atoms/featured-icon/ex-featured-icon.component'
 export * from './lib/atoms/input/input.component'
 export * from './lib/atoms/radio/radio-button.component'
 export * from './lib/atoms/search-input/search-input-dropdown.component'
+
+// Molecules
+export * from './lib/molecules/breadcrumb/breadcrumb.component'
+export * from './lib/molecules/stepper/stepper.component'
+
+// Organisms
+export * from './lib/organisms/modal/modal.component'
+export * from './lib/organisms/table/table.component'
+
+// Templates
+
+// Pages
 
 // export * from './lib/excelsior.module'
